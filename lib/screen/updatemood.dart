9a118f4bfbdb5620a4ddaceebcea3cece979f310
Redzuan Shah <cumatekan@gmail.com>
2023-03-11 01:11:27 +0800

    update updatemood.dart ui,,

--- lib/screen/updatemood.dart
+++ lib/screen/updatemood.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class UpdateMood extends StatefulWidget {
   const UpdateMood({Key? key}) : super(key: key);
@@ -8,6 +9,10 @@ class UpdateMood extends StatefulWidget {
 }
 
 class _UpdateMoodState extends State<UpdateMood> {
+  final TextEditingController moodController = TextEditingController();
+  final TextEditingController feelingController = TextEditingController();
+  final TextEditingController causeController = TextEditingController();
+  final TextEditingController description = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +20,85 @@ class _UpdateMoodState extends State<UpdateMood> {
         title: Text("Update Mood"),
         centerTitle: true,
         automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 90,
+            ),
+            Text(
+              "How's your day ?",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            RatingBar.builder(
+              initialRating: 3,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+              itemBuilder: (context, _) => Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              onRatingUpdate: (rating) {
+                print(rating);
+              },
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "What happened ?",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                controller: causeController,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Material(
+              elevation: 3,
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(40),
+              child: MaterialButton(
+                padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                onPressed: () {},
+                child: Text(
+                  "Save Mood",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
